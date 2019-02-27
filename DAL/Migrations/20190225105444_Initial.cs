5f0d5f61065edf9e0681e490f3b0219a84ac4414
Pierre Boudeaud <pboudeaud@gmail.com>
2019-02-27 10:12:40 +0100

    Create ef migrations

---
+++ DAL/Migrations/20190225105444_Initial.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Latitude = table.Column<float>(nullable: false),
+                    Longitude = table.Column<float>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    PostalCode = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Excursions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    NbPlaces = table.Column<int>(nullable: false),
+                    ActivityId = table.Column<Guid>(nullable: true),
+                    CreatorId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Excursions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Excursions_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Excursions_People_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonsExcursions",
+                columns: table => new
+                {
+                    PersonId = table.Column<Guid>(nullable: false),
+                    ExcursionId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonsExcursions", x => new { x.ExcursionId, x.PersonId });
+                    table.ForeignKey(
+                        name: "FK_PersonsExcursions_Excursions_ExcursionId",
+                        column: x => x.ExcursionId,
+                        principalTable: "Excursions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonsExcursions_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weathers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Temperature = table.Column<float>(nullable: false),
+                    WindSpeed = table.Column<float>(nullable: false),
+                    Pression = table.Column<int>(nullable: false),
+                    Humidity = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Icon = table.Column<string>(nullable: true),
+                    Sunrise = table.Column<DateTime>(nullable: false),
+                    Sunset = table.Column<DateTime>(nullable: false),
+                    ExcursionId = table.Column<Guid>(nullable: false),
+                    ExcursionId1 = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weathers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Weathers_Excursions_ExcursionId",
+                        column: x => x.ExcursionId,
+                        principalTable: "Excursions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Weathers_Excursions_ExcursionId1",
+                        column: x => x.ExcursionId1,
+                        principalTable: "Excursions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Excursions_ActivityId",
+                table: "Excursions",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Excursions_CreatorId",
+                table: "Excursions",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonsExcursions_PersonId",
+                table: "PersonsExcursions",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weathers_ExcursionId",
+                table: "Weathers",
+                column: "ExcursionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weathers_ExcursionId1",
+                table: "Weathers",
+                column: "ExcursionId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonsExcursions");
+
+            migrationBuilder.DropTable(
+                name: "Weathers");
+
+            migrationBuilder.DropTable(
+                name: "Excursions");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ DAL/Migrations/20190227085905_FixWeather.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class FixWeather : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weathers_Excursions_ExcursionId1",
+                table: "Weathers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Weathers_ExcursionId1",
+                table: "Weathers");
+
+            migrationBuilder.DropColumn(
+                name: "ExcursionId1",
+                table: "Weathers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ExcursionId1",
+                table: "Weathers",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weathers_ExcursionId1",
+                table: "Weathers",
+                column: "ExcursionId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weathers_Excursions_ExcursionId1",
+                table: "Weathers",
+                column: "ExcursionId1",
+                principalTable: "Excursions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
