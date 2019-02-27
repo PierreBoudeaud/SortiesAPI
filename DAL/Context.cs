5af57e47aafe305d462c41db70a6285fbe060117
Pierre Boudeaud <pboudeaud@gmail.com>
2019-02-27 10:03:31 +0100

    Create DAL

---
+++ DAL/Context.cs
@@ -0,0 +1,52 @@
+﻿using BO;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DAL
+{
+    public class Context: DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Excursion> Excursions { get; set; }
+
+        public DbSet<Activity> Activities { get; set; }
+
+        public DbSet<Weather> Weathers { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Excursion>()
+                .HasOne(a => a.Activity)
+                .WithMany(a => a.Excursions)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<PersonsExcursions>().HasKey(pe => new {pe.ExcursionId, pe.PersonId});
+
+            modelBuilder.Entity<PersonsExcursions>()
+                .HasOne(pe => pe.Excursion)
+                .WithMany(e => e.SubscribePeople)
+                .HasForeignKey(pe => pe.ExcursionId);
+
+            modelBuilder.Entity<PersonsExcursions>()
+                .HasOne(pe => pe.Person)
+                .WithMany(p => p.SubExcursions)
+                .HasForeignKey(pe => pe.PersonId);
+
+            modelBuilder.Entity<Excursion>()
+                .HasOne(e => e.Creator)
+                .WithMany();
+
+            modelBuilder.Entity<Excursion>()
+                .HasOne(e => e.Weather)
+                .WithOne(w => w.Excursion)
+                .HasForeignKey<Weather>(w => w.ExcursionId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DAL/GenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class GenericRepository<T> : IDisposable where T : AbstractIdentifiable
+    {
+        protected Context context;
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public Task<List<T>> GetAll()
+        {
+            return context.Set<T>().ToListAsync();
+        }
+
+        public Task<int> Update(T t)
+        {
+            context.Entry(t).State = EntityState.Modified;
+            return context.SaveChangesAsync();
+        }
+
+        public Task<int> Add(T t)
+        {
+            context.Set<T>().Add(t);
+            return context.SaveChangesAsync();
+        }
+
+        public Task<int> Delete(T t)
+        {
+            context.Set<T>().Remove(t);
+            return context.SaveChangesAsync();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            context.Set<T>().Remove(GetById(id));
+            return context.SaveChangesAsync();
+        }
+
+        public T Where(Func<T, bool> predicate)
+        {
+            return context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public List<T> WhereAll(Func<T, bool> predicate)
+        {
+            return context.Set<T>().Where(predicate).ToList();
+        }
+
+        public void Dispose()
+        {
+            context?.Dispose();
+        }
+    }
+}
